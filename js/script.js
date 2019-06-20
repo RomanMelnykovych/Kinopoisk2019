332b643f1a8822edf5f1e1137cc46a3ee1db3abf
RomanMelnykovych <roman.melnykovych97@gmail.com>
2019-06-20 11:32:51 +0300

    using Promise and then and catch

--- js/script.js
+++ js/script.js
@@ -6,35 +6,21 @@ function apiSearch(event) {
     event.preventDefault();
     const searchText = document.querySelector(".form-control").value;
     const server = "https://api.themoviedb.org/3/search/multi?api_key=a6d1d983093553c5ff65869ed9bf8cc4&language=ru&query=" + searchText;
-    requestApi('GET', server);
-}
+    movie.innerHTML = "Завантаження...";
+    requestApi("GET", server)
+        .then(function (result) {
+            const  output = JSON.parse(result);
+            console.log(output);
+            let inner = "";
 
-searchForm.addEventListener('submit', apiSearch);
+            output.results.forEach(function (item){
+                let nameItem = item.name || item.title;
+                let dateItem = item.first_air_date || item.release_date;
+                let posterItem = item.poster_path || item.backdrop_path;
+                let descriptionItem = item.overview;
+                let ratingItem = item.vote_average;
 
-function requestApi(method, url) {
-    const request = new XMLHttpRequest();
-    request.open(method, url);
-    request.send();
-
-    request.addEventListener("readystatechange", function() {
-        if (request.readyState !== 4) return;
-
-        if (request.status !== 200){
-            console.log("error: " + request.status);
-            return;
-        }
-
-        const  output = JSON.parse(request.responseText);
-        console.log(output);
-        let inner = "";
-        output.results.forEach(function (item){
-            let nameItem = item.name || item.title;
-            let dateItem = item.first_air_date || item.release_date;
-            let posterItem = item.poster_path || item.backdrop_path;
-            let descriptionItem = item.overview;
-            let ratingItem = item.vote_average;
-
-            inner += `<div class="card bg-light mb-3 w-100" >
+                inner += `<div class="card bg-light mb-3 w-100" >
                             <div class="row no-gutters">
                                 <div class="col-md-4">
                                     <img src="${getImg}${posterItem}" class="card-img" alt="poster">
@@ -49,10 +35,32 @@ function requestApi(method, url) {
                                  </div>
                             </div>
                        </div>`
+            });
+            movie.innerHTML = inner;
+        })
+        .catch(function (reason) {
+            movie.innerHTML = "Упс, щось пішло не так!!";
+            console.log("error: " + reason.status);
         });
+}
 
-        movie.innerHTML = inner;
+searchForm.addEventListener('submit', apiSearch);
 
+function requestApi(method, url) {
+    return new Promise(function (resolve, reject) {
+        const request = new XMLHttpRequest();
+        request.open(method, url);
+        request.addEventListener("load", function () {
+            if (request.status !== 200){
+                reject({status: request.status});
+                return;
+            }
+            resolve(request.response)
+        });
+        request.addEventListener("error", function () {
+            reject({status: request.status});
+        });
+        request.send();
     });
 
 }
